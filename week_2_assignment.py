cc43d94650bd5b953931a0743e5a88d51e56dc55
Johnson Emmanuel <diponee@gmail.com>
2025-01-19 17:18:19 +0100

    updated

--- week_2_assignment.py
+++ week_2_assignment.py
@@ -1,42 +1,17 @@
-password = 1234
-balance = 3000000
+password = "fight club"
 attempts = 0
 max_attempts = 3
 
-while True:
+while attempts < max_attempts:
     
-    print('Welcome to GoodNews ATM machine')
-    pawd = int(input('Enter your password to continue:'))
-    
-    if pawd == password:
+    login = str(input("Enter your password: ")).casefold()
+    print(login)
+    if login == password:
         print("Access Granted")
+        break 
         
-        print("Please choose a option")
-        print("1. Check Balance")
-        print("2. Withdraw")
-        print("3. Deposit")
-        print("4. Quit")
-        option = int(input("Enter option: ")) 
-        
-        
-        if option == 1:
-            print(f"Your balance == {balance}")
-        if option == 2:
-            amt = int(input("Enter Amount: "))
-            balance -= amt
-            print("Withdraw successful!")
-        if option == 3:
-            amt = int(input("Enter Amount: "))
-            balance += amt
-            print("Deposit successful!")
-        if option == 4:
-            print("Thank you for banking with us.")
-            break
-            
     else:
-        print("Unauthorized User, Try Again")
+        print("Unauthorized, Try Again")
         max_attempts -= 1
-        
-        if max_attempts == 0:
-            print("Too many attempts. Access Locked.")
-            break
+        if attempts == max_attempts:
+            print("Too many attempts. Access Locked.")  
